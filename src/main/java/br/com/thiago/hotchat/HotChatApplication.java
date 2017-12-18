a0d854e98ba30ea487fce0ea1b2185140b1004a1
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-12-18 21:44:30 -0200

    Cria estrutura do projeto

---
+++ src/main/java/br/com/thiago/hotchat/HotChatApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.hotchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotChatApplication.class, args);
+	}
+
+}
