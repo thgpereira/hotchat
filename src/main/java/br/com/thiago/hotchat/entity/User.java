1c327eefa199dffdd1546c3c6c08c39ccd436f0c
Thiago Azevedo Pereira <tthiagoaze@gmail.com>
2017-12-18 23:11:44 -0200

    Cria entidade de usuário

---
+++ src/main/java/br/com/thiago/hotchat/entity/User.java
@@ -0,0 +1,39 @@
+package br.com.thiago.hotchat.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -8399638623571444828L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotEmpty(message = "Campo nome é obrigatório.")
+	private String name;
+
+	@Email(message = "Informe um e-mail válido.")
+	@NotEmpty(message = "Campo e-mail é obrigatório.")
+	private String email;
+
+	@NotEmpty(message = "Campo senha é obrigatório.")
+	private String password;
+
+	private boolean online;
+
+}
