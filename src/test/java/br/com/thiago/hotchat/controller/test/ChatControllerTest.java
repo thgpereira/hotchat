5ffca81975b05ae24fa5a7c8da3f006748af4e34
Thiago Azevedo Pereira <tazevedo@ciandt.com>
2017-12-21 17:03:50 -0200

    Adiciona testes unitários para controller de chat

--- src/test/java/br/com/thiago/hotchat/controller/test/ChatControllerTest.java
+++ src/test/java/br/com/thiago/hotchat/controller/test/ChatControllerTest.java
@@ -4,23 +4,32 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Ignore;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import br.com.thiago.hotchat.builder.UserBuilder;
+import br.com.thiago.hotchat.controller.ChatController;
 import br.com.thiago.hotchat.entity.User;
 import br.com.thiago.hotchat.service.UserService;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(ChatControllerTest.class)
+@WebMvcTest(ChatController.class)
 @AutoConfigureMockMvc(secure = false)
 public class ChatControllerTest {
 
@@ -30,13 +39,30 @@ public class ChatControllerTest {
 	@MockBean
 	private UserService userService;
 
+	@Mock
+	private Authentication authentication;
+
+	@Mock
+	private SecurityContext securityContext;
+
+	@Before
+	public void configSecurity() {
+		when(securityContext.getAuthentication()).thenReturn(authentication);
+		SecurityContextHolder.setContext(securityContext);
+	}
+
 	@Test
-	@Ignore
 	public void getLoadUserLoggedReturnSucess() throws Exception {
-		User userMock = new UserBuilder().build();
 		User userMockSave = new UserBuilder().withId(1L).build();
-		when(userService.findByEmail(userMock.getEmail())).thenReturn(userMockSave);
+		when(userService.findByEmail(Mockito.anyString())).thenReturn(userMockSave);
 		mvc.perform(post("/chat/user/load").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+
+	@Test
+	public void getAllUsersReturnSucess() throws Exception {
+		List<User> usersMockSave = Arrays.asList(new UserBuilder().withId(1L).build());
+		when(userService.findAllUsersExcludeEmail(Mockito.anyString())).thenReturn(usersMockSave);
+		mvc.perform(post("/chat/users/listall").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
 	}
 
 }
